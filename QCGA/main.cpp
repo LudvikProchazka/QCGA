48d87739deeff6b8a9adbfe6b0aa98b8bf2de821
LudvikProchazka <luda.4@seznam.cz>
2024-05-24 11:31:42 +0200

    test2

--- QCGA/main.cpp
+++ QCGA/main.cpp
@@ -10,6 +10,7 @@ int main()
 {
 	//TEST FRO REPO
 	QCGA::generateGeneratingBlades();
+	QCGA obj = e1 * e2 * e3;
 	Blade a = e1 + e2;
 	std::cout << a << std::endl;
 	std::cout << (a^(-1)) << std::endl;
